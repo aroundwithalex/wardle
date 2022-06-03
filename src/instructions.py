a2e22bd8429ff39e9e6bc8ccd3b83d636c82512f
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2022-06-03 15:51:11 +0100

    Add methods for reading instructions
    Added various method to read instructions for the Wordle game.  They
    primarily focus on reading instructions and printing them to the console.

---
+++ src/instructions.py
@@ -0,0 +1,98 @@
+"""
+Contains instructions on how to play the game.
+
+This module prints instructions on how to play the game to the console. 
+It provides an indication of what the different statuses mean and how many
+attempts a user has.
+
+Typical Usage:
+    from instructions import instruct
+    instruct()
+"""
+
+from pathlib import Path
+
+def generate_instructions_path():
+    """
+    Generates path to instructions file
+
+    This method generates the path to the instructions file, so it can be
+    read and displayed on the console.
+
+    Args:
+        None
+    
+    Returns:
+        Path to instructions
+    
+    Raises:
+        None
+    """
+
+    make_instructions_path = f"{Path.cwd()}/data/instructions.txt"
+    return make_instructions_path
+
+def check_instructions():
+    """
+    Checks instructions exist at expected path.
+
+    This method ensures that the instructions exist at the expected path. If
+    they don't an exception is raised.
+
+    Args:
+        None
+    
+    Returns:
+        None
+    
+    Raises:
+        FileNotFoundError if instructions don't exist
+    """
+
+    instructions_path = generate_instructions_path()
+    if not Path(instructions_path).exists():
+        raise FileNotFoundError(f"{instructions_path} does not exist.")
+
+def read_instructions():
+    """
+    Reads instructions from file.
+
+    This method reads instructions from a specific file. It returns them, so
+    they can be printed to the console
+
+    Args: 
+        None
+    
+    Returns:
+        None
+    
+    Raises:
+        None
+    """
+
+    check_instructions()
+
+    instructions_path = generate_instructions_path()
+    with open(instructions_path, 'r', encoding='utf-8-sig') as instructions:
+        read_instruction_text = instructions.read()
+        return read_instruction_text
+
+def print_instructions():
+    """
+    Generates instructions to the console.
+
+    This method generates instructions to the console to tell the user
+    how to play the Wordle game.
+
+    Arguments:
+        None
+    
+    Returns:
+        None
+    
+    Raises:
+        None
+    """
+
+    instructions = read_instructions()
+    print(instructions)
