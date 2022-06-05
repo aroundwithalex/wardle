13e28dd347f7422a2d04da4e6a599c00bcfea070
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2022-06-05 15:13:51 +0100

    Add validate method into main
    Added the validate method into main.  If an invalid guess is submitted,
    this method prints a message, decrements the attempts count and
    continues with the loop.

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ from src.instructions import print_instructions
 
 from src.wardle import Wardle
 
-from src.display import (create_display_string, 
+from src.display import (create_display_string,
                          print_display_string,
-                         print_no_more_attempts, 
+                         print_no_more_attempts,
                          print_exact_match)
 
 def main():
@@ -24,25 +24,30 @@ def main():
     Main method for the Wardle game.
 
     This method represents the main method for the Wardle game. It contains all
-    the classes and methods required to start a game, and initialises a while 
+    the classes and methods required to start a game, and initialises a while
     loop to enable a user to keep guessing until they run out of attempts.
 
     Args:
         None
-    
+
     Returns:
         None
-    
+
     Raises:
         None
     """
-    
+
     wardle = Wardle()
     print_instructions()
 
     attempts = 6
     while attempts >= 0:
         guess_word = input("Enter your guess: ")
+
+        is_valid = wardle.validate_guess(guess_word)
+        if not is_valid:
+            attempts -= 1
+            continue
 
         if wardle.is_exact_match(guess_word):
             print_exact_match()
@@ -51,7 +56,7 @@ def main():
         if attempts == 0:
             print_no_more_attempts()
             break
-        
+
         results = wardle.check_guess(guess_word)
         result_string = create_display_string(guess_word, results)
         print_display_string(result_string)
